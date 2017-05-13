07f36215ce3d12e8df14515b28f8338f6ee8dfb9
OnishiNaoki <balance22@gmai.com>
2017-05-13 16:48:40 +0900

    add containers init

---
+++ src/containers/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+
+class App extends Component {
+    render = () => {
+        return (
+            <div>
+                <input type="text" />
+                <button>add</button>
+            </div>
+        )
+    }
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import { render } from "react-dom"
 import { Provider } from "react-redux"
 import { createStore } from "redux"
 import reducers from "./reducers"
+import App from "./containers/app"
 
 // create store with reducers
 let store = createStore(reducers)
 
 render(
     <Provider store={store}>
+      <App />
     </Provider>,
     document.getElementById("root")
 )
